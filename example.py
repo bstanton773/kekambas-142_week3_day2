bbfd3eaa1ad5e2a039925e73a34f0baa00dced28
Brian <bstanton773@gmail.com>
2024-03-05 16:21:40 -0600

    Tuesday afternoon

---
+++ example.py
@@ -0,0 +1,8 @@
+print('The example.py file has been executed')
+
+
+def greet(name):
+    print(f'Hello {name.title()}, how are you?')
+
+def leave(name):
+    print(f'Goodbye {name.title()}, it was a pleasure seeing you.')
